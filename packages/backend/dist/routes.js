1eb79997e211c06439e48e8e4ec10e2b7b202f1a
Aakash <aakash.darji20@gmail.com>
2022-04-23 17:07:20 +0530

    eslint warning fix

--- packages/backend/dist/routes.js
+++ packages/backend/dist/routes.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = __importDefault(require("express"));
+/* eslint new-cap: ["error", { "capIsNewExceptions": ["Router"] }] */
 exports.router = express_1.default.Router();
 exports.router.get('/test', (req, res) => {
     res.json({ status: 'success' });
